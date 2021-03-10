72292e7c3de2bc16c3d6cf41e36ab172171bf254
Andrey Minin <ewkjf@mail.ru>
2021-03-10 19:00:40 +0300

    added ProductOfArrayExceptSelf task, fixed README.md

--- src/com/avminin/Main.java
+++ src/com/avminin/Main.java
@@ -1,8 +1,11 @@
 package com.avminin;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new CoinChange().coinChange(new int[]{1, 2, 4, 9}, 25));
+//        System.out.println(Arrays.toString(new ProductOfArrayExceptSelf().productExceptSelf(new int[]{1, 2, 3, 4})));
+        System.out.println(Arrays.toString(new ProductOfArrayExceptSelf().productExceptSelf(new int[]{2, 3, 4, 5, 6})));
     }
 }
--- src/com/avminin/ProductOfArrayExceptSelf.java
+++ src/com/avminin/ProductOfArrayExceptSelf.java
@@ -1,4 +1,20 @@
 package com.avminin;
 
+import java.util.Arrays;
+
 public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int [] res = new int[nums.length];
+        Arrays.fill(res, 1);
+        for (int i = 0; i < nums.length - 1; ++i) {
+            res[i + 1] = res[i];
+            res[i + 1] *= nums[i];
+        }
+        int prod = 1;
+        for (int i = nums.length - 1; i > 0; --i) {
+            prod *= nums[i];
+            res[i - 1] *= prod;
+        }
+        return res;
+    }
 }
