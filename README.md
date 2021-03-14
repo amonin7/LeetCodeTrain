ac5cb212208aa7046bd61558741771f0e16cc460
Andrey Minin <ewkjf@mail.ru>
2021-03-14 23:02:01 +0300

    added CourseSchedule task

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ _CoinChange.java_ - https://leetcode.com/problems/coin-change/ \
 _ProductOfArrayExceptSelf.java_ - https://leetcode.com/problems/product-of-array-except-self/ \
 _LongestRepeatingCharacterReplacement.java_ - https://leetcode.com/problems/longest-repeating-character-replacement/ \
 _MergeIntervals.java_ - https://leetcode.com/problems/merge-intervals/ \
-_ReverseLinkedList.java_ - https://leetcode.com/problems/reverse-linked-list/
+_ReverseLinkedList.java_ - https://leetcode.com/problems/reverse-linked-list/ \
+_CourseSchedule.java_ - https://leetcode.com/problems/course-schedule/
--- src/com/avminin/Main.java
+++ src/com/avminin/Main.java
@@ -1,17 +1,8 @@
 package com.avminin;
 
-import java.util.Arrays;
-import com.avminin.ReverseLinkedList.ListNode;
-
 public class Main {
 
     public static void main(String[] args) {
-        ListNode l5 = new ListNode(5, null);
-        ListNode l4 = new ListNode(4, l5);
-        ListNode l3 = new ListNode(3, l4);
-        ListNode l2 = new ListNode(2, l3);
-        ListNode l1 = new ListNode(1, l2);
-        ListNode headNew = new ReverseLinkedList().reverseList(l1);
-        System.out.println(headNew.val);
+        System.out.println(new CourseSchedule().canFinish(2, new int[][]{{1, 0}}));
     }
 }
